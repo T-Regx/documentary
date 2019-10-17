f9d3244311d9abd13e64d76cb79f5182e7ec04b3
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2019-10-17 12:49:39 +0200

    Extract `build_params()` in `details.py`

--- src/details.py
+++ src/details.py
@@ -6,13 +6,21 @@ from src.utils import first
 
 
 def load_details(declaration: str, decorations: str, definitions: str) -> dict:
-    with open(declaration) as declaration_file:
-        with open(decorations) as decorations_file:
-            with open(definitions) as definitions_file:
-                params = __polyfill_params(__unravel_params(__inherit(json.load(declaration_file))))
-                links = __decorations_move_manual_to_link(__decorations_process_groups(json.load(decorations_file)))
-                summaries = __populate_consts(__put_name(__inherit(json.load(definitions_file))))
-                return merge_dictionaries([params, links, summaries], False)
+    with open(definitions) as definitions_file:
+        with open(declaration) as declaration_file:
+            with open(decorations) as decorations_file:
+                return build_details(
+                    json.load(definitions_file),
+                    json.load(declaration_file),
+                    json.load(decorations_file))
+
+
+def build_details(summaries: dict, params: dict, links: dict) -> dict:
+    summaries = __populate_consts(__put_name(__inherit(summaries)))
+    params = __polyfill_params(__unravel_params(__inherit(params)))
+    links = __decorations_move_manual_to_link(__decorations_process_groups(links))
+
+    return merge_dictionaries([params, links, summaries], False)
 
 
 def __polyfill_params(declaration: dict) -> dict:
