43c2674e932dd506b731f6c93c91f1b514c3349f
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2019-12-22 15:13:25 +0100

    Read `documentary` folder from current working directory, and a relative template path form argparse

--- documentary/__main__.py
+++ documentary/__main__.py
@@ -1,25 +1,40 @@
-from os import path
+from os import path, getcwd
 
-from args import parse_args
-from template import document_file
+from documentary.args import parse_args
+from documentary.template import document_file
 
 
-def document(filename: str) -> None:
-    d, basename = path.split(filename)
+def document(template_path: str, documentary_path: str) -> None:
+    d, basename = path.split(template_path)
 
     document_file(
-        template=filename,
-        output=filename,
-        declaration=path.join(d, basename + '.declaration.json'),
-        decorations=path.join(d, basename + '.decorations.json'),
-        definitions=path.join(d, basename + '.definitions.json'),
-        fragments=(path.join(d, 'fragments')),
+        template=template_path,
+        output=template_path,
+        declaration=path.join(documentary_path, basename + '.documentation', 'declaration.json'),
+        decorations=path.join(documentary_path, basename + '.documentation', 'decorations.json'),
+        definitions=path.join(documentary_path, basename + '.documentation', 'definitions.json'),
+        fragments=(path.join(documentary_path, basename + '.documentation', 'fragments')),
         include_template_tag=True,
     )
-    print("Documented file {}".format(filename))
+    print("Documented file {}".format(template_path))
+
+
+def main():
+    args = parse_args()
+
+    documentary_path = path.join(getcwd(), 'documentary')
+    template_path = path.join(getcwd(), args.template)
+
+    if not path.exists(documentary_path):
+        print('To generate a documentation, navigate to a directory with "documentary" folder')
+        return
+
+    if not path.exists(template_path):
+        print("Tried to documentation file {}, but it doesn't exist".format(template_path))
+        return
+
+    document(template_path, documentary_path)
 
 
 if __name__ == '__main__':
-    args = parse_args()
-
-    document(args.template)
+    main()
