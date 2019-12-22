f29f9424a4526fa06c62627dd679e012554269a0
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2019-12-22 15:37:02 +0100

    Read `documentation` method details by path in `documentary` folder

--- documentary/__main__.py
+++ documentary/__main__.py
@@ -5,15 +5,13 @@ from documentary.template import document_file
 
 
 def document(template_path: str, documentary_path: str) -> None:
-    d, basename = path.split(template_path)
-
     document_file(
         template=template_path,
         output=template_path,
-        declaration=path.join(documentary_path, basename + '.documentation', 'declaration.json'),
-        decorations=path.join(documentary_path, basename + '.documentation', 'decorations.json'),
-        definitions=path.join(documentary_path, basename + '.documentation', 'definitions.json'),
-        fragments=(path.join(documentary_path, basename + '.documentation', 'fragments')),
+        declaration=path.join(documentary_path, 'declaration.json'),
+        decorations=path.join(documentary_path, 'decorations.json'),
+        definitions=path.join(documentary_path, 'definitions.json'),
+        fragments=(path.join(documentary_path, 'fragments')),
         include_template_tag=True,
     )
     print("Documented file {}".format(template_path))
@@ -23,6 +21,7 @@ def main():
     args = parse_args()
 
     documentary_path = path.join(getcwd(), 'documentary')
+    method_path = path.join(getcwd(), 'documentary', args.template + '.documentation')
     template_path = path.join(getcwd(), args.template)
 
     if not path.exists(documentary_path):
@@ -33,7 +32,11 @@ def main():
         print("Tried to documentation file {}, but it doesn't exist".format(template_path))
         return
 
-    document(template_path, documentary_path)
+    if not path.exists(method_path):
+        print('To generate a documentation, navigate to a directory with "documentary" folder')
+        return
+
+    document(template_path, method_path)
 
 
 if __name__ == '__main__':
