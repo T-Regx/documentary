a19a33a5f465781b00cd54a9dda4377ace689eaa
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2019-12-31 23:48:54 +0100

    Automatically increment build version

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+from version import get_and_increment
+
 setup(name='Documentary',
-      version='1.0.0',
+      version='1.0.0-{}'.format(get_and_increment()),
       description='Tool for creating concise, complex phpDoc',
       license='MIT',
       packages=['documentary'],
