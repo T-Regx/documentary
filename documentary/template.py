eca0e9f216d3d3c08e1fece7ffe5620f222a3fe7
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-01-21 00:11:58 +0100

    Clean template.py

--- documentary/template.py
+++ documentary/template.py
@@ -1,5 +1,5 @@
 from .details.preprocess_details import load_details
-from .files import map_file, fragment_fallback, fragment, MissingFragmentException
+from .files import map_file, fragment_fallback, fragment
 from .format_comment import print_method, format_preg_method
 from .placeholder import populate
 
@@ -12,36 +12,18 @@ def document_file(documentary: str,
                   definitions: str,
                   fragments: str,
                   include_template_tag: bool) -> bool:
-    printer = Printer(
-        load_details(declaration, decorations, definitions),
-        lambda method, param: fragment_fallback(fragments, method + '.param.' + param, 'param.' + param, documentary),
-        lambda method: fragment(fragments, 'method.' + method),
-        include_template_tag)
+    details = load_details(declaration, decorations, definitions)
+
+    def _render_comment(method_name: str, indent: int):
+        return print_method(
+            details=details[method_name],
+            format_method=lambda x: format_preg_method(x) if x in details else x,
+            param_mapper=lambda p: fragment_fallback(fragments, f"{method_name}.param.{p}", f'param.{p}', documentary),
+            definition_fallback=lambda: fragment(fragments, f'method.{method_name}'),
+            include_template_tag=include_template_tag,
+            indent=indent)
 
     return map_file(
         template,
         output,
-        mapper=lambda content: populate(content, replacement=printer.print))
-
-
-class Printer:
-    def __init__(self, method_details: dict, param_mapper: callable, render_fragment: callable, template_tag: bool):
-        self.method_details = method_details
-        self.param_mapper = param_mapper
-        self.render_fragment = render_fragment
-        self.template_tag = template_tag
-
-    def print(self, method_name: str, indent: int):
-        return print_method(
-            self.method_details[method_name],
-            format_method=lambda x: format_preg_method(x) if x in self.method_details else x,
-            param_mapper=lambda param: self.param_mapper(method_name, param),
-            definition_fallback=lambda: self.map_method(method_name),
-            include_template_tag=self.template_tag,
-            indent=indent)
-
-    def map_method(self, method_name: str) -> str:
-        try:
-            return self.render_fragment(method_name)
-        except MissingFragmentException as e:
-            raise e
+        mapper=lambda content: populate(content, replacement=_render_comment))
