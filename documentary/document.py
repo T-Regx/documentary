0bffbae84d9c2d86481de038aeaf504b038630e5
Daniel Wilkowski <daniel.wilkowski@ig.com>
2020-01-24 18:13:52 +0100

    Added `document_many()` function in document.py

--- documentary/document.py
+++ documentary/document.py
@@ -2,7 +2,16 @@ from os import path
 
 from .details.preprocess_details import load_details
 from .files import map_file
+from .folder import discover_templates
 from .template import bootstrap
+
+
+def document_many(documentary_path: str, root_path: str, templates_path: str, output_path: str) -> None:
+    for template in discover_templates(templates_path, root_path, documentary_path):
+        document(documentary_path,
+                 path.join(documentary_path, template),
+                 path.join(root_path, template),
+                 path.join(output_path, template))
 
 
 def document(documentary_path: str, documentation_path: str, template_path: str, output_path: str) -> None:
--- test/tests/test_document.py
+++ test/tests/test_document.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from documentary.document import document
+from documentary.document import document, document_many
 from test.resource import resource
 from test.tmpdir import directory
 
@@ -20,3 +20,19 @@ class EndToEndTest(TestCase):
             # then
             with open(resource('expected/preg.php'), 'r') as expected:
                 self.assertEqual(expected.read(), tmp.open('preg.php'))
+
+    def test_many(self):
+        with directory() as tmp:
+            # when
+            document_many(
+                documentary_path=resource('input/documentary'),
+                root_path=resource('input'),
+                templates_path='src',
+                output_path=tmp.join('output'))
+
+            # then
+            with open(resource('expected/preg.php'), 'r') as expected:
+                self.assertEqual(expected.read(), tmp.open('output/src/SafeRegex/preg.php'))
+
+            with open(resource('expected/Pattern.php'), 'r') as expected:
+                self.assertEqual(expected.read(), tmp.open('output/src/CleanRegex/Pattern.php'))
